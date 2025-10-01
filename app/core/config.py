d248c6e52b05df8065f2fcc89d7ff4a2be144cf6
Chiho Song <songchiho777@gmail.com>
2025-10-01 16:02:30 +0900

    ENV setting and alembric setting

---
+++ app/core/config.py
@@ -0,0 +1,25 @@
+from pydantic_settings import BaseSettings
+from typing import Optional
+
+class Settings(BaseSettings):
+    ENVIRONMENT: str = "development"
+
+    DATABASE_URL: str = "sqlite:///./app.db"
+    SECRET_KEY: str = "supersecret"
+    PROJECT_NAME: str = "FastAPI Tech11 Backend"
+
+    # Azure OpenAI Configuration
+    AOAI_ENDPOINT: str = ""
+    AOAI_API_KEY: str = ""
+    AOAI_DEPLOY_GPT4O_MINI: str = "gpt-4o-mini"
+    AOAI_DEPLOY_GPT4O: str = "gpt-4o"
+    AOAI_DEPLOY_EMBED_3_LARGE: str = "text-embedding-3-large"
+    AOAI_DEPLOY_EMBED_3_SMALL: str = "text-embedding-3-small"
+    AOAI_DEPLOY_EMBED_ADA: str = "text-embedding-ada-002"
+
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+settings = Settings()
