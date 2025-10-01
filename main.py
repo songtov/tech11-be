4fc36c8617b25dac3c8e9261d57ee6d7aa6b205e
Chiho Song <songchiho777@gmail.com>
2025-10-01 14:58:39 +0900

    fastapi setting

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from tech11-be!")
+# main.py
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI with uv!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
