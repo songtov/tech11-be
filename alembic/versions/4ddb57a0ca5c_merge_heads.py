04674f42a59fe8fe311a7523ee2b25efac7574fe
songtov <songchiho777@gmail.com>
2025-10-15 13:11:28 +0900

    poe lint

--- alembic/versions/4ddb57a0ca5c_merge_heads.py
+++ alembic/versions/4ddb57a0ca5c_merge_heads.py
@@ -5,15 +5,12 @@ Revises: da486a358923, e33e53747c55
 Create Date: 2025-10-15 13:06:02.616676
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
-revision: str = '4ddb57a0ca5c'
-down_revision: Union[str, Sequence[str], None] = ('da486a358923', 'e33e53747c55')
+revision: str = "4ddb57a0ca5c"
+down_revision: Union[str, Sequence[str], None] = ("da486a358923", "e33e53747c55")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
