53535f749a33f9a5f199fe812b5edf4e552845d9
songtov <songchiho777@gmail.com>
2025-10-15 10:12:32 +0900

    Quiz using db for performance

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from .research import Research
+from .tts import TTS
+from .quiz import Quiz
+
+__all__ = ["Research", "TTS", "Quiz"]
---
+++ app/repositories/__init__.py
@@ -0,0 +1,5 @@
+from .research_repository import ResearchRepository
+from .tts_repository import TTSRepository
+from .quiz_repository import QuizRepository
+
+__all__ = ["ResearchRepository", "TTSRepository", "QuizRepository"]
