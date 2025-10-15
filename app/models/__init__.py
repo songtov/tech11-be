fd17e2a33bd85883cc5f59d267e0043e985f5e3e
Seoyeong Choi <166985182+KIN0PIOO@users.noreply.github.com>
2025-10-15 10:57:50 +0900

    summary database

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from .quiz import Quiz
+from .summary import Summary
 from .research import Research
 from .tts import TTS
 
-__all__ = ["Research", "TTS", "Quiz"]
+__all__ = ["Research", "Summary" "TTS", "Quiz"]
--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -1,5 +1,6 @@
 from .quiz_repository import QuizRepository
 from .research_repository import ResearchRepository
+from .summary_repository import SummaryRepository
 from .tts_repository import TTSRepository
 
-__all__ = ["ResearchRepository", "TTSRepository", "QuizRepository"]
+__all__ = ["ResearchRepository", "SummaryRepository", "TTSRepository", "QuizRepository"]
